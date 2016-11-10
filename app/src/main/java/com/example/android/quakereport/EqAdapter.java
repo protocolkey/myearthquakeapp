7cb99dcc48c155a7d243ff3297bc44499c2e39ea
“Don” <protocolkey@gmail.com>
2016-11-09 20:43:14 -0500

    changes visual polish

--- app/src/main/java/com/example/android/quakereport/EqAdapter.java
+++ app/src/main/java/com/example/android/quakereport/EqAdapter.java
@@ -1,12 +1,13 @@
 package com.example.android.quakereport;
 
 import android.content.Context;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
-
+import android.graphics.drawable.GradientDrawable;
 import java.util.ArrayList;
 
 /**
@@ -34,8 +35,12 @@ public class EqAdapter extends ArrayAdapter <clsEQData>{
         TextView txtSecondLoc = (TextView) LstView.findViewById(R.id.txtSecondLoc);
         TextView txtDate = (TextView) LstView.findViewById(R.id.txtDate);
         TextView txtTime = (TextView) LstView.findViewById(R.id.txtTime);
+        // draw circle
+        GradientDrawable magnitudeCircle =  (GradientDrawable) txtMag.getBackground();
+        int magnitudeColor = getMagnitudeColor(ThisEqData.getMag());
+        magnitudeCircle.setColor(magnitudeColor);
 
-
+        ///-----------------------------
         txtMag.setText(ThisEqData.getMag());
         txtFirstLoc.setText(ThisEqData.getFirst_Loc());
         txtSecondLoc.setText(ThisEqData.getSecond_Loc());
@@ -45,6 +50,46 @@ public class EqAdapter extends ArrayAdapter <clsEQData>{
 
 
         return LstView;
+
+    }
+
+    private int getMagnitudeColor(String mag) {
+        int Mag = Double.valueOf(mag).intValue();
+        int ReturnVal=0;
+        switch (Mag) {
+            case 0:
+            case 1:
+                ReturnVal=  R.color.magnitude1;
+                break;
+            case 2:
+                ReturnVal= R.color.magnitude2;
+                break;
+            case 3:
+                ReturnVal= R.color.magnitude3;
+                break;
+            case 4:
+                ReturnVal= R.color.magnitude4;
+                break;
+            case 5:
+                ReturnVal= R.color.magnitude5;
+                break;
+            case 6:
+                ReturnVal= R.color.magnitude6;
+                break;
+            case 7:
+                ReturnVal= R.color.magnitude7;
+                break;
+            case 8:
+                ReturnVal= R.color.magnitude8;
+                break;
+            case 9:
+                ReturnVal= R.color.magnitude9;
+                break;
+            default:
+                ReturnVal= R.color.magnitude10plus;
+                break;
+        }
+        return ContextCompat.getColor(getContext(), ReturnVal);
 
     }
 }
